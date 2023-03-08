8301257560c385b53fee444503dad6adb77431b3
angierobayo9809 <angierobayo09@gmail.com>
2023-03-07 22:12:32 -0500

    Ejercicio 6

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
-let cantidadMax = 0;
-let inicio = 1;
-
-while (cantidadMax < 100)
+let numeros = prompt("Ingrese la cantidad de números a evaluar");
+let suma = new Number;
+let producto=new Number;
+for (let x = 1; x <= Number(numeros); x++)
 {
-        if (inicio % 2 == 0)
-        {
-            console.log(inicio);
-            cantidadMax++;
-        }
-    inicio++;
+    let num = prompt("Ingrese el número "+x);
+    if (Number(num) % 2 == 0){
+        suma +=Number(num);
+    }
+    else{
+        if (x == 1)
+            producto = Number(num);
+        else
+            producto = producto * Number(num);
+    }
 }
+
+alert("La suma de los numeros pares ingresados es de:"+suma +"\n\r El producto de los impares es de:"+producto);
